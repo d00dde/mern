3fc2da564aaeb875ee11a8cc0ba67f42d00ef986
Name <Email>
2020-01-11 06:16:36 +0200

    initial project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require ('config');
+
+const PORT = config.get('port') || 5000;
+
+app.use('/api/auth/', require('./routes/auth.route'));
+
+(async function () {
+  try {
+    mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    app.listen(PORT, () => console.log(`App has been srarted on ${PORT} ...`));
+    
+  } catch (e) {
+    console.log('Server error:', e.message);
+    process.exit(1);
+  }
+})();
+
+
---
+++ routes/auth.route.js
@@ -0,0 +1,25 @@
+const {Router} = require ('express');
+const User = require('../models/User');
+const router = Router();
+
+// api/auth/register/
+router.post('register/', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    
+
+  } catch (e) {
+    res.status(500).json({message: e.message})
+  }
+});
+
+// api/auth/login/
+router.post('login/', async (req, res) => {
+  try {
+    
+  } catch (e) {
+    res.status(500).json({message: e.message})
+  }
+});
+
+module.exports = router;
